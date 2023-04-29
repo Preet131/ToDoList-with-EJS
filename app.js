40bd55d30d3979d292397be84223485e73c1f7f8
Preet131 <90319247+Preet131@users.noreply.github.com>
2023-04-29 11:57:19 +0530

    second version

--- app.js
+++ app.js
@@ -6,38 +6,36 @@ const bodyParser = require("body-parser");
 // Initialize Express
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine",  "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/", function(req, res){
 
-var today = new Date();
-var currentDay = today.getDay();
-var day = "";
+let today = new Date();
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+}
+let day = today.toLocaleDateString("en-US", options);
 
 
-if(currentDay == 1){
-    day = "Monday";
-}
-else if(currentDay == 2){
-    day = "Tuesday";
-}
-else if(currentDay == 3){
-    day = "Wednesday";
-}
-else if(currentDay == 4){
-    day = "Thursday";
-}
-else if(currentDay == 5){
-    day = "Friday";
-}
-else {
-    day = "Weekend";
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    });
+});
 
-}
-    res.render("list", {kindOfDay: day});
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
     console.log("Server started on http://localhost:3000");
     
-})
+});
